3ad52f020ea296025dc3d33806c0307834bfae5b
Oleh Bilostotskiy <89212792+OlehWhite@users.noreply.github.com>
2025-10-02 18:18:32 +0300

    Upload other files

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import MainLayout from "@/layouts/MainLayout";
+import { ReactNode } from "react";
+
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Bilostotskyi Oleh - Front-End Developer / Webflow Specialist",
+  description: "",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,67 @@
+import MainLayout from "@/layouts/MainLayout";
+import { ENavLinks } from "@/types/nav.types";
+import Image from "next/image";
+import { HERO } from "@/constants/home";
+import Button from "@/components/Button";
+import { SOCIAL_MEDIAS } from "@/constants/socials-medias";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <MainLayout page={ENavLinks.HOME}>
+      <section className="flex-wrap flex justify-between md:flex-nowrap">
+        <div className="w-full max-w-[750px]">
+          <h1 className="text-6xl mb-0">{HERO.hello}</h1>
+          <h2>
+            <span className="text-3xl text-primary">
+              {HERO.technologies.frontend}
+            </span>{" "}
+            /{" "}
+            <span className="text-3xl text-secondary">
+              {HERO.technologies.webflow}
+            </span>
+          </h2>
+          <p className="text-lg">{HERO.text}</p>
+
+          <div className="flex gap-6">
+            {/*<Button color="main" >Hire Me</Button>*/}
+
+            {/*<Button color="secondary" >Let's Talk</Button>*/}
+            <Button href="mailto:oleg22098d@gmail.com">Let's Talk</Button>
+          </div>
+
+          <div className="flex gap-3 mt-[100px]">
+            {SOCIAL_MEDIAS.map((el, index) => (
+              <Link
+                key={index}
+                href={el.link}
+                target="_blank"
+                className="cursor-pointer"
+              >
+                <Image
+                  src={el.image}
+                  width={30}
+                  height={30}
+                  alt={el.title}
+                  title={el.title}
+                />
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        <div className="relative w-[420px] h-[500px]">
+          <Image
+            src="/images/oleh-bilostotskyi.jpg"
+            alt="Main Logo"
+            title="Main Logo"
+            className="rounded-xl object-cover"
+            fill
+          />
+        </div>
+      </section>
+    </MainLayout>
+  );
+};
+
+export default Home;
