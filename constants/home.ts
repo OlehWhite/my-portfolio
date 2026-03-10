904ce6cfb342fe45169df6d791f5068132496f31
Oleh Bilostotskiy <89212792+OlehWhite@users.noreply.github.com>
2026-03-10 13:18:05 +0200

    Update home.ts

--- constants/home.ts
+++ constants/home.ts
@@ -7,7 +7,7 @@ export const HERO = {
   text: [
     "I help startups, agencies, and business owners launch stunning, responsive websites that look amazing and perform even better.",
     "Whether you need a sleek landing page, a dynamic CMS-powered blog, or a full corporate website — I’ve got you covered.",
-    "I also have 3+ years of experience with - React | Next.js | Java Script | Type Script | HTML | CSS",
+    "I also have around 4 years of experience with - React | Next.js | Java Script | Type Script | HTML | CSS",
   ],
   intro: "https://www.youtube.com/embed/zp4meXqVCIg?autoplay=1",
 };
